1f151f09f1f3a327dea048df14e33f9fc395c1d1
r1v3ra-1508 <dsd436938@gmail.com>
2025-10-14 22:29:43 +0300

    added: nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd.colorscheme('catppuccin')
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.wrap = false
---
+++ .config/nvim/lua/config/plugins.lua
@@ -0,0 +1,13 @@
+return {
+
+-- catppuccin
+
+{
+
+"catppuccin/nvim",
+
+name = "catppuccin",
+
+priority = 1000
+
+}
