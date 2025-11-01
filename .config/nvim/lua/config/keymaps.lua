61a9ddde35fc950cc8923b3bbddda088e9f213e7
r1v3ra-1508 <dsd436938@gmail.com>
2025-11-01 19:08:54 +0300

    feat (nvim): discord rpc, nvim-tree and keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+map({ 'n', 'i' }, '<C-r>', '<cmd>undo<cr>', { desc = 'Undo' })
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr>", { desc = "Save File" })
+map({ "i", "x", "n", "s" }, "<C-t>", "<cmd>NvimTreeFocus<cr>", { desc = "Focus nvim-tree" })
+map({ "n" }, "<tab>q", "<cmd>tab previous<cr>", { desc = "Previous tab" })
+map({ "n" }, "<tab>e", "<cmd>tab next<cr>", { desc = "Next tab" })
+map({ "n" }, "<tab>c", "<cmd>tab close<cr>", { desc = "Close tab" })
+map({ "n" }, "<tab>n", "<cmd>tab new<cr>", { desc = "New tab" })
